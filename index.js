c33f0691c2a246930db88d4ffcfdb270de0e8987
Jackie Sze <jackiesze0@gmail.com>
2020-02-18 20:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function makeInt(string) {
-  parseInt('7', 10)
+  parseInt('7', 10);
+  return makeInt(string);
 }
 
 function preserveDecimal(string) {
-  
+  parseFloat('9.183');
+  return preserveDecimal(string);
 }
+
+makeInt(string)
