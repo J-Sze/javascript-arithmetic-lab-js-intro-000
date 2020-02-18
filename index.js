f3956c59ef0543bc4d06aa827afec3f2f0fefa35
Jackie Sze <jackiesze0@gmail.com>
2020-02-18 21:02:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(x, y) {
   add(x + y);
 }
+
+function subtract(x, y) {
+  subtract(x - y);
+}
+
+function multiply(x, y) {
+  multiply(x * y);
+}
+
+function divide(x, y) {
+  divide(x/y);
+}
